c9a5131fee027990a7cc10263d9b1bcd157f04cb
Stefano Riccio <riccio.stefano0@gmail.com>
2019-12-19 16:24:10 +0100

    iniziato lavoro JS

--- js/script.js
+++ js/script.js
@@ -1,40 +1,59 @@
-// Calcolo prezzo del treno:
-// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere
-var numeroKm = parseInt(prompt('Quanti Km devi percorrere?'));
-console.log (numeroKm);
- // e l’età del passeggero.
 
-var anni = parseInt(prompt('Quanti anni hai?')) ;
-console.log (anni);
+var buttonGenera = document.getElementById('genera');
+buttonGenera.addEventListener ('click',
+function() {
+  // Calcolo prezzo del treno:
+  // Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere
+  // var numeroKm = parseInt(prompt('Quanti Km devi percorrere?'));
+  // console.log (numeroKm);
+  //  // e l’età del passeggero.
+  //
+  // var anni = parseInt(prompt('Quanti anni hai?')) ;
+  // console.log (anni);
+  //
+  // // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+  //
+  // // Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+  // var euroKm = 0.21;
+  // var prezzoKm = (numeroKm * euroKm).toFixed(2);
+  // console.log (prezzoKm);
+  // // ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+  // var over = 65;
+  // var under = 18;
+  //
+  // if (anni >= over) {
+  //   console.log (prezzoKm - ((prezzoKm * 40) / 100).toFixed(2));
+  //
+  //   document.getElementById('prezzo_biglietto').innerHTML = (prezzoKm - ((prezzoKm * 40) / 100).toFixed(2)) + "€";
+  // }
+  // else if (anni < under){
+  //   console.log (prezzoKm - ((prezzoKm *20) / 100).toFixed(2));
+  //
+  //   document.getElementById('prezzo_biglietto').innerHTML = (prezzoKm - ((prezzoKm *20) / 100).toFixed(2)) + "€";
+  // }
+  // else {
+  //   console.log (prezzoKm);
+  //
+  //   document.getElementById('prezzo_biglietto').innerHTML = (prezzoKm) + "€";
+  // }
+  //
+  // // stampa su foglio html
+  // // km
+  // document.getElementById('km').innerHTML = numeroKm;
+  // // età
+  // document.getElementById('età').innerHTML = anni;
 
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+  // ESERCIZIO ODIERNO
+  var inputKm = document.getElementById('km');
+  console.log(inputKm);
 
-// Il prezzo del biglietto è definito in base ai km (0.21 € al km)
-var euroKm = 0.21;
-var prezzoKm = (numeroKm * euroKm).toFixed(2);
-console.log (prezzoKm);
-// ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
-var over = 65;
-var under = 18;
+  var kmDaPercorrere = parseInt(inputKm.value);
+  console.log(kmDaPercorrere);
 
-if (anni >= over) {
-  console.log (prezzoKm - ((prezzoKm * 40) / 100).toFixed(2));
+  var inputNome = document.getElementById('nome');
+  console.log(inputNome);
 
-  document.getElementById('prezzo_biglietto').innerHTML = (prezzoKm - ((prezzoKm * 40) / 100).toFixed(2)) + "€";
+  var nomeUtente = inputNome.value;
+  console.log(nomeUtente);
 }
-else if (anni < under){
-  console.log (prezzoKm - ((prezzoKm *20) / 100).toFixed(2));
-
-  document.getElementById('prezzo_biglietto').innerHTML = (prezzoKm - ((prezzoKm *20) / 100).toFixed(2)) + "€";
-}
-else {
-  console.log (prezzoKm);
-
-  document.getElementById('prezzo_biglietto').innerHTML = (prezzoKm) + "€";
-}
-
-// stampa su foglio html
-// km
-document.getElementById('km').innerHTML = numeroKm;
-// età
-document.getElementById('età').innerHTML = anni;
+)
